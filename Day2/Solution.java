7cb4e5ab211d6267e84034b5a7eee6a7e47fedd1
TealStone <ultratealstone@gmail.com>
2020-12-02 16:57:39 -0500

    Somehow did things in wrong folder

--- Day2/Solution.java
+++ Day2/Solution.java
@@ -1,4 +1,4 @@
-package Day1;
+package Day2;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -6,18 +6,11 @@ import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) throws FileNotFoundException {
-        Scanner scan = new Scanner(new File("Day1/input.txt"));
+        Scanner scan = new Scanner(new File("Day2/input.txt"));
         int answer = 0;
         int answer2 = 0;
         while(scan.hasNextLine()){
-            String line = scan.nextLine();
-            int number = Integer.parseInt(line);
-            number = number / 3 - 2;
-            answer += number;
-            while(number > 0){
-                answer2 += number;
-                number = number / 3 - 2;
-            }
+
         }
         System.out.println(answer);
         System.out.println(answer2);
