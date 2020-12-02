b06b1a1ce1272429efc1eade6f84a609cd51f18d
TealStone <ultratealstone@gmail.com>
2020-12-02 16:53:26 -0500

    Finished Day1 and changed template

---
+++ Day1/Solution.java
@@ -0,0 +1,15 @@
+package Day1;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner scan = new Scanner(new File("Day1/input.txt"));
+
+        while(scan.hasNextLine()){
+            String line = scan.nextLine();
+        }
+    }
+}
--- Template/Solution.java
+++ Template/Solution.java
@@ -6,10 +6,20 @@ import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) throws FileNotFoundException {
-        Scanner scan = new Scanner(new File("Day2/input.txt"));
-
+        Scanner scan = new Scanner(new File("Day1/input.txt"));
+        int answer = 0;
+        int answer2 = 0;
         while(scan.hasNextLine()){
             String line = scan.nextLine();
+            int number = Integer.parseInt(line);
+            number = number / 3 - 2;
+            answer += number;
+            while(number > 0){
+                answer2 += number;
+                number = number / 3 - 2;
+            }
         }
+        System.out.println(answer);
+        System.out.println(answer2);
     }
 }
